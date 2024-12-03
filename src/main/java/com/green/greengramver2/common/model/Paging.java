0de2000533e3614158c7e052fb85cd095ae1b4ac
jangjaeung <wodnd1998@gmail.com>
2024-12-03 16:02:39 +0900

    24.12.03 [피드 댓글 버그 수정] [완료]

--- src/main/java/com/green/greengramver2/common/model/Paging.java
+++ src/main/java/com/green/greengramver2/common/model/Paging.java
@@ -9,19 +9,17 @@ import lombok.ToString;
 @Getter
 @ToString
 public class Paging {
-    @JsonIgnore
-    private final static int DEFAULT = 10;
-    @Schema(example = "30", description = "item count per page")
-    private int page;
     @Schema(example = "1", description = "Selected Page")
+    private int page;
+    @Schema(example = "30", description = "item count per page")
     private int size;
     @JsonIgnore
     private int startIdx;
 
-    public Paging(Integer size , Integer page){
-        this.page = page==null || page<=0? 1 : page;
-        this.size = size==null || size<=0? DEFAULT : size;
+    public Paging(Integer page, Integer size) {
+        this.page = (page == null || page <= 0) ? 1 : page;
         this.size = (size == null || size <= 0) ? Constants.getDefault_page_size() : size;
+        this.startIdx = ( this.page - 1 ) * this.size;
     }
     //setter 만들면 위에 생성자에서 page값이 들어왔다면 그건 보존하고 없는 값인 size값을 처리해준다.
     //굳이 setter랑 생성자 같이 만들필요 없음
